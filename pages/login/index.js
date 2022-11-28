fc87f9af6b6830cd29a0dad838cce11ad04928b8
PIYUSH MANI CHAUBEY <42182675+MANIPIYUSH@users.noreply.github.com>
2022-11-28 19:23:23 +0530

    added

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import TextField from '@mui/material/TextField';
+import React ,{useContext,useEffect,useState} from 'react'
+import { TextField } from '@mui/material';
+
 import Image from "next/image";
 import insta from "../../assets/insta.jpg"
 import Button from '@mui/material/Button';
@@ -7,9 +8,39 @@ import { Carousel } from 'react-responsive-carousel';
 import bg1 from "../../assets/bg1.jpg"
 import bg2 from "../../assets/bg2.jpg"
 import bg3 from "../../assets/bg3.jpg"
-
-
+import {carouselProvider,Slider,Slide,Image as Img} from "pure-react-carousel";
+import {useRouter} from 'next/router';
+import {AuthContext} from "../../context/auth"
 function index() {
+const router = useRouter()     //conditional routing
+const [email,setEmail]=React.useState('')
+const [password,setPassword] = React.useState('')
+const [error,setError] = React.useState('')
+const [loading,setLoading]=React.useState(false)
+const{login,user} =useContext(AuthContext)
+const handleClick = async() =>{
+     try{
+          setLoading(true)
+          setError('')
+          await login(email,password)
+          console.log("Logged in")
+     }catch(err){
+          console.log(err)
+          setError(err.message)
+          setTimeout(()=>{
+               setError('')
+          },2000)
+     }
+     setLoading(false)
+}
+useEffect(() => {
+     if(user){
+          router.push('/')
+     }else{
+          console.log("Not Logged in");
+     }     
+},  [user]);
+
   return (
     <div className='login-container'>
          <div className='carbg'>
@@ -30,21 +61,21 @@ function index() {
      <div>
       <div className='login-card'>
         <Image src={insta}/>
-         <TextField id="outlined-basic" fullWidth margin='dense' size='small' label="E-Mail" variant="outlined" />
-         <TextField id="outlined-basic" fullWidth margin='dense' size='small' label="Password" type="password" variant="outlined" />
-      
-        <div>
-           <span style={{color:"red", marginTop:"0.5rem"}}>error</span>
-        </div>
-      <Button variant="contained" fullWidth component="span" style={{marginTop:"1rem"}}  >
+         <TextField id="outlined-basic" fullWidth margin='dense' size='small' label="E-Mail" variant="outlined" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+         <TextField id="outlined-basic" fullWidth margin='dense' size='small' label="Password" type="password" variant="outlined" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+      {
+          errror != '' && 
+          <div style={{color:'red'}}>{error}</div>
+      }
+      <Button variant="contained" fullWidth component="span" style={{marginTop:"1rem"}} onClick={handleClick} disabled={loading}>
            Log In
       </Button>
       <div>
            <span style={{color:"blue", marginTop:"0.5rem"}}>Forgot Password?</span>
            </div>
-      </div>
+      </div> 
            <div className='bottom-card'>
-            Dont't Have an Account?<span style={{color:"blue"}}>Log In</span>
+            Dont't Have an Account?<span style={{color:"blue"}}>Sign Up</span>
            </div>
            </div>
       </div>
